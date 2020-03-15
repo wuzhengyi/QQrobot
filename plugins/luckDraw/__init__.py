0591e86158ab02d657a36b690e33129f2bbb8311
wzy <wuzy.nju@gmail.com>
2020-03-15 10:21:34 +0800

    更新插件架构

--- plugins/luckDraw/__init__.py
+++ plugins/luckDraw/__init__.py
@@ -1,12 +1,7 @@
 import nonebot
 from nonebot import on_command, CommandSession
+from datasource import userSQL, getRandom, isRoot
 from os import path
-import sqlite3
-import random
-
-seq = [1350, 710, 288, 50, 30, 20, 10, 8, 5]
-prob = [0.0001, 0.002, 0.0109, 0.03, 0.06, 0.327, 0.46, 0.1, 0.01]
-rootList = [942858979, 914349145]
 
 # on_command 装饰器将函数声明为一个命令处理器
 # 这里 luckyDraw 为命令的名字
@@ -74,73 +69,3 @@ async def TopScore(session: CommandSession):
             break
     message = '积分排行榜\n' + ''.join(['Top '+str(i+1)+'. '+QQname[i]+'\t '+str(rank[i][1])+'\n' for i in range(len(QQname))])
     await session.send(message)
-
-def getRandom() -> int:
-    x = random.uniform(0, 1)
-    cumprob = 0.0
-    for item, item_pro in zip(seq, prob):
-        cumprob += item_pro
-        if x < cumprob:
-            break
-    return item
-
-def isRoot(QQ:int) -> bool:
-    return QQ in rootList
-
-class userSQL():
-    def __init__(self):
-        self.conn = sqlite3.connect('E:\\酷Q Air\\data\\app\\top.zls520.jf\\user.db')
-        self.c = self.conn.cursor()
-        
-    def insert(self, QQ: int) -> None:
-        self.c.execute("INSERT INTO USER (QQ,JF,TICKET) \
-                        VALUES ("+str(QQ)+", 0, 0)")
-
-    def getScore(self, QQ: int) -> int:
-        self.c.execute("select JF from user where QQ=" + str(QQ))
-        ans = self.c.fetchall()
-        if ans == []:
-            self.insert(QQ)
-            return 0
-        return ans[0][0]
-    
-    def getTicket(self, QQ: int) -> int:
-        self.c.execute("select Ticket from user where QQ=" + str(QQ))
-        ans = self.c.fetchall()
-        if ans == []:
-            self.insert(QQ)
-            return 0
-        if ans[0][0] == None:
-            self.c.execute("UPDATE user set ticket = 0 where ticket is null")
-            return 0
-        return ans[0][0]
-
-    def getTopTicket(self) -> [(int, int)]:
-        self.c.execute("UPDATE user set ticket = 0 where ticket is null")
-        self.c.execute("select QQ, Ticket from user ORDER BY TICKET DESC")
-        ans = self.c.fetchall()
-        return ans
-
-    def getTopScore(self) -> [(int, int)]:
-        self.c.execute("UPDATE user set jf = 0 where jf is null")
-        self.c.execute("select QQ, jf from user ORDER BY jf DESC")
-        ans = self.c.fetchall()
-        return ans
-
-    def subScore(self, QQ:int, value:int) -> None:
-        self.c.execute("UPDATE user set jf = jf-"+str(value)+" where QQ=" + str(QQ))
-    
-    def addScore(self, QQ:int, value:int) -> None:
-        self.c.execute("UPDATE user set jf = jf+"+str(value)+" where QQ=" + str(QQ))
-
-    def subTicket(self, QQ:int, value:int) -> None:
-        self.getTicket(QQ)
-        self.c.execute("UPDATE user set ticket = ticket-"+str(value)+" where QQ=" + str(QQ))
-    
-    def addTicket(self, QQ:int, value:int) -> None:
-        self.getTicket(QQ)
-        self.c.execute("UPDATE user set ticket = ticket+"+str(value)+" where QQ=" + str(QQ))
-
-    def close(self):
-        self.conn.commit()
-        self.conn.close()
---
+++ plugins/luckDraw/datasource.py
@@ -0,0 +1,77 @@
+import sqlite3
+import random
+
+rootList = [942858979, 914349145]
+
+
+def getRandom() -> int:
+    seq = [1350, 710, 288, 50, 30, 20, 10, 8, 5]
+    prob = [0.0001, 0.002, 0.0109, 0.03, 0.06, 0.327, 0.46, 0.1, 0.01]
+    x = random.uniform(0, 1)
+    cumprob = 0.0
+    for item, item_pro in zip(seq, prob):
+        cumprob += item_pro
+        if x < cumprob:
+            break
+    return item
+
+def isRoot(QQ:int) -> bool:
+    return QQ in rootList
+
+class userSQL():
+    def __init__(self):
+        self.conn = sqlite3.connect('E:\\酷Q Air\\data\\app\\top.zls520.jf\\user.db')
+        self.c = self.conn.cursor()
+        
+    def insert(self, QQ: int) -> None:
+        self.c.execute("INSERT INTO USER (QQ,JF,TICKET) \
+                        VALUES ("+str(QQ)+", 0, 0)")
+
+    def getScore(self, QQ: int) -> int:
+        self.c.execute("select JF from user where QQ=" + str(QQ))
+        ans = self.c.fetchall()
+        if ans == []:
+            self.insert(QQ)
+            return 0
+        return ans[0][0]
+    
+    def getTicket(self, QQ: int) -> int:
+        self.c.execute("select Ticket from user where QQ=" + str(QQ))
+        ans = self.c.fetchall()
+        if ans == []:
+            self.insert(QQ)
+            return 0
+        if ans[0][0] == None:
+            self.c.execute("UPDATE user set ticket = 0 where ticket is null")
+            return 0
+        return ans[0][0]
+
+    def getTopTicket(self) -> [(int, int)]:
+        self.c.execute("UPDATE user set ticket = 0 where ticket is null")
+        self.c.execute("select QQ, Ticket from user ORDER BY TICKET DESC")
+        ans = self.c.fetchall()
+        return ans
+
+    def getTopScore(self) -> [(int, int)]:
+        self.c.execute("UPDATE user set jf = 0 where jf is null")
+        self.c.execute("select QQ, jf from user ORDER BY jf DESC")
+        ans = self.c.fetchall()
+        return ans
+
+    def subScore(self, QQ:int, value:int) -> None:
+        self.c.execute("UPDATE user set jf = jf-"+str(value)+" where QQ=" + str(QQ))
+    
+    def addScore(self, QQ:int, value:int) -> None:
+        self.c.execute("UPDATE user set jf = jf+"+str(value)+" where QQ=" + str(QQ))
+
+    def subTicket(self, QQ:int, value:int) -> None:
+        self.getTicket(QQ)
+        self.c.execute("UPDATE user set ticket = ticket-"+str(value)+" where QQ=" + str(QQ))
+    
+    def addTicket(self, QQ:int, value:int) -> None:
+        self.getTicket(QQ)
+        self.c.execute("UPDATE user set ticket = ticket+"+str(value)+" where QQ=" + str(QQ))
+
+    def close(self):
+        self.conn.commit()
+        self.conn.close()
