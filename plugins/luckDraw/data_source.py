0bbb7d1f359152465d32d5a2311cd760f29eac56
wzy <wuzy.nju@gmail.com>
2020-03-16 09:31:19 +0800

    Update data_source.py
    修复白羊座bug

--- plugins/luckDraw/data_source.py
+++ plugins/luckDraw/data_source.py
@@ -8,7 +8,7 @@ pokeName_database = ['xiaolada', 'bobo', 'miaomiao', 'wasidan', 'apashe',
 #                     '大舌头', '皮卡丘', '皮皮', '胖丁', '伊布', '吉利蛋', '袋龙', '梦幻']
 # pokeName = zip(pokeName_database, pokeName_chinese)
 
-cons_database = ['Alies', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo',
+cons_database = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo',
                  'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
 # cons_chinese = ['白羊座', '金牛座', '双子座', '巨蟹座', '狮子座',
 #                 '处女座', '天秤座', '天蝎座', '射手座', '摩羯座', '水瓶座', '双鱼座']
