bdae4ee1655b5ed056087b9ee8bf32757f0fd0eb
wzy <wuzy.nju@gmail.com>
2020-03-19 16:18:28 +0800

    完成版本迭代v2.2

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from nonebot.default_config import *
 
-SUPERUSERS = {914349145, 942858979}
+# SUPERUSERS = {914349145, 942858979}
+SUPERUSERS = {942858979}
 COMMAND_START = {'', '/', '!', '／', '！'}
 DEBUG = False
