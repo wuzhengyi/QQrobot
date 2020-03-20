5686526d68534b03160edc17b268a114c5f3be5f
wzy <wuzy.nju@gmail.com>
2020-03-20 22:55:22 +0800

    增加下车和换位置功能

--- plugins/HonorOfKingsTeam/__init__.py
+++ plugins/HonorOfKingsTeam/__init__.py
@@ -7,8 +7,12 @@ __plugin_name__ = '王者组队'
 __plugin_usage__ = r"""
 命令如下：
 /开车 或 /[王者|空]组队 会创建一个空的队伍
-艾特我并且发送位置可以上车
+
+/[游戏位置] 发送位置可以上车
 位置有：打野 中单 战士 辅助 射手
+
+/换位置 [打野|中单|战士|辅助|射手]
+/下车
 """
 
 game = {'打野': None, '中单': None, '战士': None, '辅助': None, '射手': None}
@@ -21,8 +25,10 @@ def echoTeam(game: {}) -> str:
 def isFullTeam(game: {}) -> bool:
     return all([game[x] for x in game])
 
-def isInTeam(game:{}, name:str) -> bool:
+
+def isInTeam(game: {}, name: str) -> bool:
     return not all([game[x] != name for x in game])
+
 
 @on_command('开车', aliases=('组队', '王者组队'), only_to_me=False)
 async def createTeam(session: CommandSession):
@@ -49,7 +55,7 @@ async def jungle(session: CommandSession):
         await session.send(f'开车成功，快进入游戏吧！当前队伍:\n{echoTeam(game)}')
 
 
-@on_command('战士', aliases=('边路','上单',), only_to_me=False)
+@on_command('战士', aliases=('边路', '上单',), only_to_me=False)
 async def warrior(session: CommandSession):
     global game
     if game['战士'] == None:
@@ -119,3 +125,37 @@ async def shooter(session: CommandSession):
         await session.send(f'上车失败，已经有无敌射手啦，要不要试一试别的位置呀，当前队伍:\n{echoTeam(game)}')
     if isFullTeam(game):
         await session.send(f'开车成功，快进入游戏吧！当前队伍:\n{echoTeam(game)}')
+
+
+@on_command('换位置', only_to_me=False)
+async def changePlace(session: CommandSession):
+    global game
+    inpt = session.state.get('message') or session.current_arg
+    args = inpt.strip()
+    bot = nonebot.get_bot()
+    member = await bot.get_group_member_info(group_id=session.ctx['group_id'], user_id=session.ctx['user_id'])
+    name = member['card'] or member['nickname']
+    if args not in game:
+        await session.send('对不起哦，你想换的位置不存在。')
+        return
+    if not isInTeam(game, name):
+        await session.send('对不起哦，当前您不在我们的小车车上。')
+        return
+    for x in game:
+        if game[x] == name:
+            game[x] = None
+    game[args] = name
+    await session.send(f'换位置成功！当前队伍:\n{echoTeam(game)}')
+
+
+@on_command('下车', only_to_me=False)
+async def leaveGame(session: CommandSession):
+    global game
+    bot = nonebot.get_bot()
+    member = await bot.get_group_member_info(group_id=session.ctx['group_id'], user_id=session.ctx['user_id'])
+    name = member['card'] or member['nickname']
+    if isInTeam(game, name):
+        for x in game:
+            if game[x] == name:
+                game[x] = None
+        await session.send('下车成功。')
