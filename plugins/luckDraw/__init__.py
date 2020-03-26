b74384a8424e7bf2b22de1ffa9ee07e2633848aa
NJU-Justice <18805156360@163.com>
2020-03-26 19:44:24 +0800

    Revert "优化物品显示"
    This reverts commit 4e739af5b3cfde73c4b0561a926543a8d4a19939.

--- plugins/luckDraw/__init__.py
+++ plugins/luckDraw/__init__.py
@@ -167,13 +167,12 @@ async def backpack(session: CommandSession):
 
     user.close()
 
-    message = '{0:-^26}\n'.format('我的背包') + \
-              '{}{}:{}\t{}{}:{}\n{}{}:{}\n'.format(
-        getBallEmoji('evelsBall'), '精灵球', evelsBall,
-        getBallEmoji('superBall'), '超级球', superBall,
-        getBallEmoji('masterBall'), '大师球:', masterBall) + \
-        ''.join(['{}{}:{}\t{}{}:{}\n'.format(getConsEmoji(i), consName[i], cons[i],
-                                             getConsEmoji(i+1), consName[i+1], cons[i+1]) for i in range(0, len(cons), 2)])
+    message = '我的背包' + '\n' + \
+              getBallEmoji('evelsBall') + '精灵球:' + str(evelsBall) + '\n' + \
+              getBallEmoji('superBall') + '超级球:' + str(superBall) + '\n' + \
+              getBallEmoji('masterBall') + '大师球:' + str(masterBall) + '\n' + \
+              ''.join([getConsEmoji(i) + consName[i] + ':' + str(cons[i]) +
+                       '\n' for i in range(len(cons))])
     await session.send(message[:-1])
 
 
@@ -190,17 +189,18 @@ async def pet(session: CommandSession):
     menghuanIndex = pokeNameChn.index('梦幻')
     chaomengInedx = pokeNameChn.index('超梦')
     message = '{0:-^26}\n'.format('我的宠物')
-    message = message + ''.join(['{0:{4}<4}:{1:>2}{4}\t{2:{4}<4}:{3:>2}\n'.format(
-        pokeNameChn[i], pokemon[i],pokeNameChn[i + 1], pokemon[i + 1],
-        chr(12288)) for i in range(0, menghuanIndex, 2)])
-    message = message + '{0:{2}<4}:{1:>2}\n'.format(pokeNameChn[menghuanIndex], 
-        pokemon[menghuanIndex], chr(12288)) if pokemon[menghuanIndex] != 0 else message
-    message = message + ''.join(['{0:{4}<4}:{1:>2}{4}\t{2:{4}<4}:{3:>2}\n'.format(
-        pokeNameChn[i], pokemon[i],pokeNameChn[i + 1], pokemon[i + 1], 
-        chr(12288)) for i in range(menghuanIndex + 1, chaomengInedx - 1, 2)])
+    message = message + ''.join(['{0:{4}<4}:{1:>2}{4}\t{2:{4}<4}:{3:>2}\n'.format(pokeNameChn[i], pokemon[i],
+                                                                                    pokeNameChn[i + 1], pokemon[i + 1],
+                                                                                    chr(12288)) for i in
+                                 range(0, menghuanIndex, 2)])
+    message = message + '{0:{2}<4}:{1:>2}\n'.format(
+        pokeNameChn[menghuanIndex], pokemon[menghuanIndex], chr(12288)) if pokemon[menghuanIndex] != 0 else message
+    message = message + ''.join(['{0:{4}<4}:{1:>2}{4}\t{2:{4}<4}:{3:>2}\n'.format(pokeNameChn[i], pokemon[i],
+                                                                                    pokeNameChn[i + 1], pokemon[i + 1],
+                                                                                    chr(12288)) for i in
+                                 range(menghuanIndex + 1, chaomengInedx - 1, 2)])
     message = message + '{0:{4}<4}:{1:>2}{4}\t{2:{4}<4}:{3:>2}\n'.format(
-        pokeNameChn[chaomengInedx - 1], pokemon[chaomengInedx - 1], 
-        pokeNameChn[chaomengInedx], pokemon[chaomengInedx],
+        pokeNameChn[chaomengInedx - 1], pokemon[chaomengInedx - 1], pokeNameChn[chaomengInedx], pokemon[chaomengInedx],
         chr(12288)) if pokemon[chaomengInedx] != 0 else message + '{0:{2}<4}:{1:>2}\n'.format(
         pokeNameChn[chaomengInedx - 1], pokemon[chaomengInedx - 1], chr(12288))
     await session.send(message[:-1])
@@ -318,7 +318,7 @@ async def diamonDraw(session: CommandSession):
         if ticketNum > 0:
             user.addTicket(QQ, ticketNum)
             message = message + getImage('ticket') + \
-                '\n恭喜你获得' + str(ticketNum) + '奖券\n'
+                      '\n恭喜你获得' + str(ticketNum) + '奖券\n'
         cardNum = ans.count('card')
         if cardNum > 0:
             cardIndex = [random.randint(0, 11) for i in range(cardNum)]
@@ -327,34 +327,34 @@ async def diamonDraw(session: CommandSession):
                 if num > 0:
                     user.addCons(QQ, i, num)
                     message = message + getConsImage(i) + '\n恭喜你获得' + \
-                        str(num) + '张' + consName[i] + '卡片\n'
+                              str(num) + '张' + consName[i] + '卡片\n'
         diamondSum = sum([int(i) for i in ans if i.isdigit()])
         if diamondSum > 0:
             user.addDiamond(QQ, diamondSum)
             if diamondSum in [30, 60, 120, 300]:
                 message = message + \
-                    getImage(str(diamondSum) + 'diamond') + \
-                    '\n恭喜你获得' + str(diamondSum) + '钻石\n'
+                          getImage(str(diamondSum) + 'diamond') + \
+                          '\n恭喜你获得' + str(diamondSum) + '钻石\n'
             else:
                 message = message + '恭喜你获得' + str(diamondSum) + '钻石\n'
         evelsBall = ans.count('evelsBall')
         if evelsBall > 0:
             user.addEvelsBall(QQ, evelsBall)
             message = message + \
-                getBallImage('evelsBall') + '\n恭喜你获得' + \
-                str(evelsBall) + '个精灵球\n'
+                      getBallImage('evelsBall') + '\n恭喜你获得' + \
+                      str(evelsBall) + '个精灵球\n'
         superBall = ans.count('superBall')
         if superBall > 0:
             user.addSuperBall(QQ, superBall)
             message = message + \
-                getBallImage('superBall') + '\n恭喜你获得' + \
-                str(superBall) + '个超级球\n'
+                      getBallImage('superBall') + '\n恭喜你获得' + \
+                      str(superBall) + '个超级球\n'
         masterBall = ans.count('masterBall')
         if masterBall > 0:
             user.addMasterBall(QQ, masterBall)
             message = message + \
-                getBallImage('masterBall') + '\n恭喜你获得' + \
-                str(masterBall) + '个大师球\n欢迎下次光临。'
+                      getBallImage('masterBall') + '\n恭喜你获得' + \
+                      str(masterBall) + '个大师球\n欢迎下次光临。'
         await session.send(message)
     else:
         await session.send('你的钻石不足，快去找老蛋赚钻石吧。')
